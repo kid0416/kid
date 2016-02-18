1356f7c2cb3150965e1dcf71aa14aebe901e254f
lixin <lixin@111.com.cn>
2016-02-18 17:59:09 +0800

    commit test class

---
+++ src/com/kid/test/FirstTestClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.kid.test;
+
+/**
+ * @author lixin
+ * 2016年2月18日
+ * @since
+ * @version 1.0
+ */
+public class FirstTestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello github...");
+	}
+
+}
